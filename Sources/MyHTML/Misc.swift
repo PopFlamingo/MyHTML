6f14a67abf7775b271300cc5300a911b9aca0e41
Trevör Anne Denise <trevor.annedenise@icloud.com>
2019-01-17 18:30:36 +0100

    Make myhtml_namespace_t Equatable

--- Sources/MyHTML/Misc.swift
+++ Sources/MyHTML/Misc.swift
@@ -1,5 +1,12 @@
+import CMyHTML
+
 enum Error: Swift.Error {
     case cannotCreateBaseStructure
     case statusError(rawValue: UInt32)
 }
 
+extension myhtml_namespace_t: Equatable {
+    public static func ==(lhs: myhtml_namespace_t, rhs: myhtml_namespace_t) -> Bool {
+        return lhs.rawValue == rhs.rawValue
+    }
+}
