1ac7be69c717054c85451b72158f67ff482eabe0
Trevör Anne Denise <trevor.annedenise@icloud.com>
2019-01-18 01:59:39 +0100

    Add correct access modifiers

--- Sources/MyHTML/Misc.swift
+++ Sources/MyHTML/Misc.swift
@@ -1,11 +1,11 @@
 import CMyHTML
 
-enum Error: Swift.Error {
+public enum Error: Swift.Error {
     case cannotCreateBaseStructure
     case statusError(rawValue: UInt32)
 }
 
-extension myhtml_namespace_t: Equatable {
+public extension myhtml_namespace_t: Equatable {
     public static func ==(lhs: myhtml_namespace_t, rhs: myhtml_namespace_t) -> Bool {
         return lhs.rawValue == rhs.rawValue
     }
